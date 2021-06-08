653334329977388c5ac6b425e93b8fea9d163007
Jin <jin.dev.1425@gmail.com>
2021-06-07 20:49:34 -0400

    Implement Cat read route middlewares

--- controllers/cats.js
+++ controllers/cats.js
@@ -4,6 +4,36 @@ const Cat = require('../models/Cat');
 const asyncHandler = require('../utils/asyncHandler');
 const ErrorResponse = require('../utils/ErrorResponse');
 
+// @desc      Get all cats
+// @route     Get /api/v1/cats
+// @access    Public
+exports.getCats = asyncHandler(async (req, res, next) => {
+  const cats = await Cat.find();
+
+  res.status(200).send({
+    success: true,
+    data: cats
+  });
+});
+
+// @desc      Get a cat by id
+// @route     Get /api/v1/cats/:id
+// @access    Public
+exports.getCat = asyncHandler(async (req, res, next) => {
+  const cat = await Cat.findById(req.params.id);
+
+  if (!cat) {
+    return next(
+      new ErrorResponse(`Cat not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).send({
+    success: true,
+    data: cat
+  });
+});
+
 // @desc      Add a new cat
 // @route     Post /api/v1/cats
 // @access    Private
--- routers/cats.js
+++ routers/cats.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
-const { createCat, uploadCatImage } = require('../controllers/cats');
+const {
+  getCats,
+  getCat,
+  createCat,
+  uploadCatImage
+} = require('../controllers/cats');
 
 const router = express.Router();
 
-router.route('/').post(createCat);
+router.route('/').get(getCats).post(createCat);
+
+router.route('/:id').get(getCat);
 
 router.route('/:id/photo').post(uploadCatImage);
 
