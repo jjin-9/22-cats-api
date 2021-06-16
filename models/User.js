09add4c762a4d7210e8c1c677f53eb35675d7d45
Jin <jin@jin.local>
2021-06-15 23:26:00 -0400

    Implement forgotPassword and resetPassword routes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const validator = require('validator');
@@ -22,6 +23,8 @@ const UserSchema = new mongoose.Schema({
     },
     select: false
   },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
   role: {
     type: String,
     enum: ['user'],
@@ -49,6 +52,23 @@ UserSchema.methods.getJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE
   });
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model('User', UserSchema);
