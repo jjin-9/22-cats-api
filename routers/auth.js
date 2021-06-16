8c71c564ae3c39946a8114319f9fe18d44dd7838
Jin <jin@jin.local>
2021-06-15 23:32:01 -0400

    Set up a route for forgotpassword and resetpassword

--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
-const { register, login } = require('../controllers/auth');
+const {
+  register,
+  login,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/forgotpassword', forgotPassword);
+router.post('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
